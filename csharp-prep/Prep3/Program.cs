deb3f9bf1b2e196bff6ddc2c594b9a32617b272f
Matthew Jensen <jen220757@byui.edu>
2024-01-13 16:58:07 -0700

    added a guess the number game. The computer generates a random number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 100);
+        int number = -1;
+
+        while (number != randomNumber)
+        {
+            Console.Write("Please guess a number. ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > randomNumber)
+                Console.WriteLine("Lower!");
+                
+            else if (number < randomNumber)
+                Console.WriteLine("Higher!");
+            else
+                Console.WriteLine("Congrats you guessed the number!");
+        }
     }
 }
