0438dc8cba89ebef282fe806632e6b6c6ce67629
Matthew Jensen <jen220757@byui.edu>
2024-01-13 18:47:04 -0700

    added functions that display welcome, prompt user name, prompt user number, square number, and display the result.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        displayWelcome();
+
+        string name = promptUserName();
+        int userNumber = promptUserNumber();
+
+        int squared = squareNumber(userNumber);
+
+        displayResult(name, userNumber, squared);
+
+        static void displayWelcome()
+        {
+        Console.WriteLine("Welcome to the program!");
+        }
+
+        static string promptUserName()
+        {
+            Console.WriteLine("Please enter your first name: ");
+            string userName = Console.ReadLine();
+
+            return userName;
+        }
+    
+        static int promptUserNumber()
+        {
+            Console.WriteLine("Please enter your favorite number! ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int squareNumber(int number)
+        {
+            int squared = number * number;
+            return squared;
+        }
+        
+        static void displayResult(string userName, int number, int squared)
+        {
+            Console.WriteLine($"{userName} The square of your number {number} is {squared}.");
+        }
     }
 }
